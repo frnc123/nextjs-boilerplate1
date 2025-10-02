cf47c111ea3765029fb017d3420963ea8110a2c4
Lail <pageoflail@gmail.com>
2025-10-02 10:42:20 +0700

    ganti nama

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,28 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/sza.jpg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
+        <h1><b>Kelompok Kecebur Laut</b></h1>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
+          <li className="tracking-[-.01em]">
+            Ferincia Diarra N. Z.
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Layla Nur Fitria
+          </li>
+          <li className="tracking-[-.01em]">
+            Shafa Early
           </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-[#0385a] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
